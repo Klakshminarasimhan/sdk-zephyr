037e70c9aabb01c749dd71998b435b7ae2764f38
klakshminarasimhan <klakshminarasimhan@logitech.com>
2024-08-05 15:06:34 +0530

    changes for ep_exp in hid sample ep1 64bytes ep2:3kb ep3:64bytes

--- samples/subsys/usb/hid-mouse/src/main.c
+++ samples/subsys/usb/hid-mouse/src/main.c
@@ -22,8 +22,86 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
+#define HID_MOUSE_REPORT_DESC1(bcnt)                                                                             \
+    {                                                                                                                 \
+        HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP), HID_USAGE(HID_USAGE_GEN_DESKTOP_MOUSE),                                \
+            HID_COLLECTION(HID_COLLECTION_APPLICATION), HID_USAGE(HID_USAGE_GEN_DESKTOP_POINTER),                     \
+            HID_COLLECTION(HID_COLLECTION_PHYSICAL), HID_REPORT_COUNT(bcnt), HID_REPORT_SIZE(1), HID_LOGICAL_MIN8(0), \
+            HID_LOGICAL_MAX8(1), HID_USAGE_PAGE(HID_USAGE_GEN_BUTTON), HID_USAGE_MIN8(1), HID_USAGE_MAX8(bcnt),       \
+            HID_INPUT(0x02), HID_REPORT_COUNT(2), HID_REPORT_SIZE(16), 0x16, 0x01, 0x80, 0x26, 0xff, 0x7F,            \
+            HID_USAGE_PAGE(HID_USAGE_GEN_DESKTOP), HID_USAGE(HID_USAGE_GEN_DESKTOP_X),                                \
+            HID_USAGE(HID_USAGE_GEN_DESKTOP_Y), HID_INPUT(6), HID_REPORT_COUNT(1), HID_REPORT_SIZE(8), 0x15, 0x81,    \
+            HID_LOGICAL_MAX8(127), HID_USAGE(HID_USAGE_GEN_DESKTOP_WHEEL), 0x81, 0x06, 0x95, 0x01, 0x05, 0x0c, 0x0a,  \
+            0x38, 0x02, 0x81, 0x06, HID_END_COLLECTION, 0x06, 0x00, 0xff, 0x09, 0xf1, HID_REPORT_SIZE(8),             \
+            HID_REPORT_COUNT(5), HID_LOGICAL_MIN8(0), HID_LOGICAL_MAX8(255), 0x81, 0x00, 0x06, 0x01,                  \
+            0xff,             /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x02,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x03,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xF4,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+                                                                                                                      \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x04,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x04,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x04,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0x02,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x05,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x06,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x07,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x08,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x05,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0x04,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+\
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x05,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x06,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x07,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x08,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0xFF,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            0x06, 0x01, 0xff, /*   USAGE_PAGE (Vendor Defined Page 2) */                                              \
+            0x09, 0x05,       /*   USAGE (Vendor Usage 2) */                                                          \
+            0x95, 0x04,       /*   REPORT_COUNT (7) */                                                                \
+            0x81, 0x02,       /*   OUTPUT (Data,Var,Abs) */                                                           \
+            HID_END_COLLECTION,                                                                                       \
+    }
+
+
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
-static const uint8_t hid_report_desc[] = HID_MOUSE_REPORT_DESC(2);
+static const uint8_t hid_report_desc[] =  HID_MOUSE_REPORT_DESC1(16);
 static enum usb_dc_status_code usb_status;
 
 #define MOUSE_BTN_LEFT		0
@@ -34,11 +112,11 @@ enum mouse_report_idx {
 	MOUSE_X_REPORT_IDX = 1,
 	MOUSE_Y_REPORT_IDX = 2,
 	MOUSE_WHEEL_REPORT_IDX = 3,
-	MOUSE_REPORT_COUNT = 4,
+	MOUSE_REPORT_COUNT = 3072,
 };
 
-K_MSGQ_DEFINE(mouse_msgq, MOUSE_REPORT_COUNT, 2, 1);
-static K_SEM_DEFINE(ep_write_sem, 0, 1);
+static uint8_t __aligned(sizeof(void *)) report[MOUSE_REPORT_COUNT];
+static K_SEM_DEFINE(report_sem, 0, 1);
 
 static inline void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 {
@@ -86,13 +164,10 @@ static void input_cb(struct input_event *evt)
 		return;
 	}
 
-	if (k_msgq_put(&mouse_msgq, tmp, K_NO_WAIT) != 0) {
-		LOG_ERR("Failed to put new input event");
+	if (memcmp(tmp, report, sizeof(tmp))) {
+		memcpy(report, tmp, sizeof(report));
+		k_sem_give(&report_sem);
 	}
-
-	tmp[MOUSE_X_REPORT_IDX] = 0U;
-	tmp[MOUSE_Y_REPORT_IDX] = 0U;
-
 }
 
 INPUT_CALLBACK_DEFINE(NULL, input_cb);
@@ -121,19 +196,9 @@ static int enable_usb_device_next(void)
 }
 #endif /* IS_ENABLED(CONFIG_USB_DEVICE_STACK_NEXT) */
 
-static void int_in_ready_cb(const struct device *dev)
-{
-	ARG_UNUSED(dev);
-	k_sem_give(&ep_write_sem);
-}
-
-static const struct hid_ops ops = {
-	.int_in_ready = int_in_ready_cb,
-};
-
 int main(void)
 {
-	const struct device *hid_dev;
+	const struct device *hid_dev,*hid_dev1,*hid_dev2;
 	int ret;
 
 	if (!gpio_is_ready_dt(&led0)) {
@@ -142,7 +207,10 @@ int main(void)
 	}
 
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
-	hid_dev = DEVICE_DT_GET_ONE(zephyr_hid_device);
+	// hid_dev = DEVICE_DT_GET_ONE(zephyr_hid_device);
+	hid_dev = device_get_binding("hid_dev_0");
+	hid_dev1 = device_get_binding("hid_dev_1");
+	hid_dev2 = device_get_binding("hid_dev_2");
 #else
 	hid_dev = device_get_binding("HID_0");
 #endif
@@ -159,10 +227,14 @@ int main(void)
 
 	usb_hid_register_device(hid_dev,
 				hid_report_desc, sizeof(hid_report_desc),
-				&ops);
-
+				NULL);
+	usb_hid_register_device(hid_dev1,
+				hid_report_desc, sizeof(hid_report_desc),
+				NULL);
+	usb_hid_register_device(hid_dev2, hid_report_desc, sizeof(hid_report_desc), NULL);				
 	usb_hid_init(hid_dev);
-
+	usb_hid_init(hid_dev1);
+	usb_hid_init(hid_dev2);
 #if defined(CONFIG_USB_DEVICE_STACK_NEXT)
 	ret = enable_usb_device_next();
 #else
@@ -174,17 +246,19 @@ int main(void)
 	}
 
 	while (true) {
-		UDC_STATIC_BUF_DEFINE(report, MOUSE_REPORT_COUNT);
+		// k_sem_take(&report_sem, K_FOREVER);
 
-		k_msgq_get(&mouse_msgq, &report, K_FOREVER);
-
-		ret = hid_int_ep_write(hid_dev, report, MOUSE_REPORT_COUNT, NULL);
+		ret = hid_int_ep_write(hid_dev, report, 64, NULL);
+		ret = hid_int_ep_write(hid_dev1, report, sizeof(report), NULL);
+		ret = hid_int_ep_write(hid_dev2, report, 64, NULL);
 		if (ret) {
 			LOG_ERR("HID write error, %d", ret);
-		} else {
-			k_sem_take(&ep_write_sem, K_FOREVER);
-			/* Toggle LED on sent report */
-			(void)gpio_pin_toggle(led0.port, led0.pin);
+		}
+
+		/* Toggle LED on sent report */
+		ret = gpio_pin_toggle(led0.port, led0.pin);
+		if (ret < 0) {
+			LOG_ERR("Failed to toggle the LED pin, error: %d", ret);
 		}
 	}
 	return 0;
